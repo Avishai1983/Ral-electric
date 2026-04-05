7a9e0ec9d75fd639e19e8dba277861e8ce7f4771
Avishai1983 <Avishai1983@gmail.com>
2026-04-05 15:16:31 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'ral-electric-v1';
+const FILES = [
+  '/Ral-electric/',
+  '/Ral-electric/index.html',
+  '/Ral-electric/Ral.html',
+  '/Ral-electric/manifest.json',
+  '/Ral-electric/icon-192.png',
+  '/Ral-electric/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
